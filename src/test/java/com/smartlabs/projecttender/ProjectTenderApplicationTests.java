361d8328169a8cf2281439ef02b3cfd63bfe1994
Fernando Guerrero <devrrior@gmail.com>
2023-03-22 22:41:11 -0600

    Set up project

---
+++ src/test/java/com/smartlabs/projecttender/ProjectTenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartlabs.projecttender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectTenderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
