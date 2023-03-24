f6d6d077bfca88a63b692273cf406704a1096cb0
rodrick <213480@ids.upchiapas.edu.mx>
2023-03-24 01:10:18 -0600

    feat project and user entities

--- src/main/java/com/smartlabs/projecttender/entities/User.java
+++ src/main/java/com/smartlabs/projecttender/entities/User.java
@@ -1,4 +1,13 @@
 package com.smartlabs.projecttender.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
 public class User {
 }
